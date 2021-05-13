b3b8576102829ed047b31764088a540700bea557
kaveri9850 <kaveri9850@gmail.com>
2021-05-13 17:32:42 +0530

    Project added

---
+++ TestJenkinsAndgit/src/Demo/Demo1.java
@@ -0,0 +1,12 @@
+package Demo;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hiiii jenkins");
+		System.out.println("hello");
+
+	}
+
+}
