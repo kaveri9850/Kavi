65c84e62f281d9b053ed515de42036a536363a02
kaveri9850 <kaveri9850@gmail.com>
2021-05-13 17:48:04 +0530

    added new package

---
+++ NewJenkins/src/Demo3/Test.java
@@ -0,0 +1,11 @@
+package Demo3;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hiiiii git hub and jenkins");
+
+	}
+
+}
