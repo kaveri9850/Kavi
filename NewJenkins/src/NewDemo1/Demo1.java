f240d34aaafaaef55d0638ae5810058292f47406
kaveri9850 <kaveri9850@gmail.com>
2021-05-13 17:39:38 +0530

    New project added

---
+++ NewJenkins/src/NewDemo1/Demo1.java
@@ -0,0 +1,12 @@
+package NewDemo1;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hiiii jenkins");
+		System.out.println("hello");
+
+	}
+
+}
